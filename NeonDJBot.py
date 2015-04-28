44ee4567ffe5780f8d3a455f2723a40ef66a8a2c
Daniel Drexler <ddrexler@bitcasa.com>
2015-04-27 22:14:28 -0700

    Test for updating in place

--- NeonDJBot.py
+++ NeonDJBot.py
@@ -9,7 +9,7 @@ from actions import SetNickAction
 class NeonDJBot(Bot):
 	@asyncio.coroutine
 	def setup(self):
-		yield from self.action_queue.put((self.TAG_DO_ACTION, SetNickAction("♬|NeonDJBot")))
+		yield from self.action_queue.put((self.TAG_DO_ACTION, SetNickAction("♬|NeonDJBot|Test")))
 
 #bot = NeonDJBot('ws://localhost:8765/')
 bot = NeonDJBot('wss://euphoria.io/room/music/ws')
